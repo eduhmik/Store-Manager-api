4d726d82ad1e676360f2f8b639e801247d87002d
Eduhmik <edwinkimaita78@gmail.com>
2018-10-16 14:14:45 +0300

    [Chore #161214750] finished writing tests for getting all products

---
+++ app/tests/V1/base_test.py
@@ -0,0 +1,22 @@
+"""
+This will contain the configuration to be reused in all tests.
+"""
+# Library imports 
+from unittest import TestCase
+from app.storemanager import create_app
+from instance.config import app_config
+
+
+
+class BaseTest(TestCase):
+    """
+    Class to hold all similar test config
+    """
+    def setUp(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
